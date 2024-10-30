1e597951bca40ccd89cae79220338b32a58cfe00
tjjames230 <108950500+tjjames230@users.noreply.github.com>
2024-10-30 11:08:53 -0500

    FIX(previewEducation): updated header to reflect school on save

--- src/components/PreviewEducation.jsx
+++ src/components/PreviewEducation.jsx
@@ -4,7 +4,7 @@ const PreviewEducation = ({ education }) => {
 	const listEducation = education.map((educationItem) => {
 		return (
 			<div key={educationItem.study}>
-				<h3>test</h3>
+				<h3>{educationItem.school}</h3>
 				<p>{educationItem.study}</p>
 				{educationItem.startDate && (
 					<p>{formatDate(educationItem.startDate)}</p>
